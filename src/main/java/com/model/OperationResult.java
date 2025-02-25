826d20ffe9e2ea3a2e5aefe127b6c11b45d4a9ec
mcarey02005 <mcarey02005@gmail.com>
2025-02-25 12:50:49 -0500

    Implement OperationResult

---
+++ src/main/java/com/model/OperationResult.java
@@ -0,0 +1,46 @@
+package com.model;
+
+/**
+ * Describes the result of any operation including data return, errors, and comments regarding the operation.
+ * 
+ * @author Matt Carey
+ */
+
+public class OperationResult<T> {
+    public final boolean success;
+    public final T result;
+    public final Exception error;
+    public final String message;
+
+    /**
+     * Constructs a new successful operation result, with the return value of result.
+     * @param result
+     */
+    public OperationResult(T result) {
+        this.success = true;
+        this.result = result;
+        this.error = null;
+        this.message = null;
+    }
+    /**
+     * Constructs a new unsuccessful operation result specifying a message and exception
+     * @param message Developer generated note
+     * @param error Java generated error
+     */
+    public OperationResult(String message, Exception error) {
+        this.success = false;
+        this.result = null;
+        this.error = error;
+        this.message = message;
+    }
+    /**
+     * Constructs a new unsuccessful operation result specifying a message
+     * @param message Developer generated note
+     */
+    public OperationResult(String message) {
+        this.success = false;
+        this.result = null;
+        this.error = null;
+        this.message = message;
+    }
+}
