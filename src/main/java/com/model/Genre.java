46dd9e7311eff295c1594ba293f0c83c2997728e
mad1826 <mad18@email.sc.edu>
2025-02-28 19:50:23 -0500

    Resolve Create Genre #12

---
+++ src/main/java/com/model/Genre.java
@@ -0,0 +1,25 @@
+package com.model;
+
+/**
+ * A genre that songs may belong to
+ * @author Michael Davis
+ */
+public enum Genre {
+	POP,
+	METAL,
+	ROCK,
+	HIPHOP,
+	BLUES,
+	FOLK,
+	JAZZ,
+	COUNTRY,
+	CLASSICAL,
+	SOUL,
+	FUNK,
+	DISCO,
+	DUBSTEP,
+	ELECTRONICA,
+	RANDB,
+	REGGAE,
+	LOFI
+}
