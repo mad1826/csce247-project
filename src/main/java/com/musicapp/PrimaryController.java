5e4ab15a5fbd2c0c836560cc8fffdfac7f120ded
mad1826 <mad18@email.sc.edu>
2025-04-10 12:39:07 -0400

    Resolve #187

--- src/main/java/com/musicapp/PrimaryController.java
+++ src/main/java/com/musicapp/PrimaryController.java
@@ -1,12 +1,48 @@
 package com.musicapp;
 
 import java.io.IOException;
+
+import com.model.MusicAppFacade;
+import com.model.User;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    private Button btnlogin;
+
+    @FXML
+    private Label output;
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    private TextField textPassword;
+
+    @FXML
+    private TextField textUsername;
+
+    @FXML
+    void login(ActionEvent event) {
+		MusicAppFacade facade = MusicAppFacade.getInstance();
+		facade.login(textUsername.getText(), textPassword.getText());
+		User user = facade.getCurrentUser();
+		if (user == null) {
+			output.setText("User not found");
+		}
+		else {
+			output.setText("Signed in as " + user.getFirstName() + " " + user.getLastName());
+		}
+    }
+
+    @FXML
+    void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 }
