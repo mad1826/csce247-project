53123a6fd31498ae39aa1bcf6f34d0519335f792
Squiddleton <mothmain9515@outlook.com>
2025-04-10 23:36:45 -0400

    Resolve #199

--- src/main/java/com/musicapp/App.java
+++ src/main/java/com/musicapp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 540, 960);
+        scene = new Scene(loadFXML("login"), 450, 800);
         stage.setScene(scene);
         stage.show();
     }
