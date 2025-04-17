d9b27251f5d7ea851c96c2aecb27b5a558e3b8a3
mcarey02005 <mcarey02005@gmail.com>
2025-04-17 13:30:18 -0400

    Joining Courses

--- src/main/java/com/controllers/CourseController.java
+++ src/main/java/com/controllers/CourseController.java
@@ -1,31 +1,86 @@
 package com.controllers;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 import com.model.Course;
-import com.model.Lesson;
 import com.model.MusicAppFacade;
-import com.model.Student;
+import com.model.OperationResult;
 import com.model.User;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class CourseController extends NavigatableController {
     @FXML
     private Label welcome;
     
+	@FXML
+	private Button joinCourse;
+
     @FXML
 	private VBox listCourses;
 
 	private MusicAppFacade facade;
 
 	private User user;
+
+	private void promptJoinCourse() {
+		Stage popup = new Stage();
+		popup.initModality(Modality.APPLICATION_MODAL); // blocks input to other windows
+		popup.setTitle("Join a Course");
+
+		VBox mainContainer = new VBox();
+		HBox lowerOptions = new HBox();
+
+		TextField input = new TextField();
+		input.setPromptText("Course ID");
+
+		Label feedback = new Label("");
+
+		Button closeButton = new Button("Cancel");
+		closeButton.setOnAction(e -> {
+			System.out.println("CLOSE?");
+			popup.close();
+		});
+
+		Button joinButton = new Button("Join");
+		joinButton.setOnAction(e -> {
+			String text = input.getText();
+			OperationResult<Course> or = facade.joinCourse(text);
+			System.out.println(or);
+			if (or.success) {
+				popup.close();
+
+				loadCourses(); //redisplay courses
+			} else { //display issue
+				feedback.setText(or.message);
+
+				PauseTransition delay = new PauseTransition(javafx.util.Duration.seconds(3));
+				delay.setOnFinished(event -> feedback.setText(""));
+				delay.play();
+			}
+		});
+
+		lowerOptions.getChildren().addAll(closeButton,joinButton);
+
+		mainContainer.getChildren().addAll(input,lowerOptions,feedback);
+
+		Scene popupScene = new Scene(mainContainer, 200, 100);
+
+		popup.setScene(popupScene);
+		popup.showAndWait(); // blocks until popup is closed
+	}
 
     @FXML
 	@Override
@@ -36,6 +91,8 @@ public class CourseController extends NavigatableController {
 		user = facade.getCurrentUser();
         welcome.setText("My Courses");
 
+		joinCourse.setOnAction(e -> promptJoinCourse());
+
 		loadCourses();
     }
 
@@ -43,6 +100,7 @@ public class CourseController extends NavigatableController {
 	 * Loads all lessons for the user's courses.
 	 */
 	private void loadCourses() {
+		listCourses.getChildren().clear();
 		for (Course course : user.getCourses()) {
 			listCourses.getChildren().add(getLessonVBox(course));
 		}
